ce278722eb7b14a32d713ac885317770e77b5a9d
Xiao <xmeng1@ualberta.ca>
2021-04-12 22:05:15 -0600

    add function for prompting player

--- code.js
+++ code.js
@@ -1,13 +1,23 @@
 function computerPlay() {
     const random_number = Math.floor(Math.random()*3)+1;
     console.log(random_number);
-    if (random_number == 1){
+    if (random_number ===1){
         return "rock";
     }
-    else if (random_number == 2) {
+    else if (random_number === 2) {
         return "paper";
     }
     else {
         return "scissors";
+    }
+}
+
+function playerPlay() {
+    while (true){
+        const player_selection = prompt("Choose rock, paper, or scissors.").toLowerCase();
+        if (player_selection === "rock" || player_selection === "paper" || 
+                player_selection === "scissors"){
+            return player_selection;
+        }
     }
 }
