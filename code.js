85b781ead3ebe4c3c0f236e5fbe18d7b2f7ecda2
Xiao <xmeng1@ualberta.ca>
2021-04-12 21:44:19 -0600

    add computer_play function

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
 function computerPlay() {
-    random_number = Math.floor(Math.random()*3)+1;
-    console.log(random_number)
+    const random_number = Math.floor(Math.random()*3)+1;
+    console.log(random_number);
+    if (random_number == 1){
+        return "rock";
+    }
+    else if (random_number == 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
 }
