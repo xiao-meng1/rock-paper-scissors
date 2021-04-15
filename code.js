b6174f38e55599f8055b156a41c8b11bf470f322
Xiao <xmeng1@ualberta.ca>
2021-04-14 21:55:26 -0600

    add single round and 5 round game functions

--- code.js
+++ code.js
@@ -1,6 +1,5 @@
 function computerPlay() {
     const random_number = Math.floor(Math.random()*3)+1;
-    console.log(random_number);
     if (random_number ===1){
         return "rock";
     }
@@ -12,12 +11,80 @@ function computerPlay() {
     }
 }
 
-function playerPlay() {
+function userPlay() {
     while (true){
         const player_selection = prompt("Choose rock, paper, or scissors.").toLowerCase();
         if (player_selection === "rock" || player_selection === "paper" || 
                 player_selection === "scissors"){
             return player_selection;
         }
+        else {
+            alert('Please type "rock", "paper", or "scissors"!')
+        }
     }
+}
+
+function playRound() {
+    const userSelection = userPlay();
+    const computerSelection = computerPlay();
+    let winner;
+    switch (true) {
+        case userSelection === "rock" && computerSelection === "rock":
+            console.log("Tie! Both players chose rock");
+            winner = "tie";
+            break;
+        case userSelection === "rock" && computerSelection === "paper":
+            console.log("You lose! Paper beats rock");
+            winner = "computer";
+            break;
+        case userSelection === "rock" && computerSelection === "scissors":
+            console.log("You win! Rock beats scissors");
+            winner = "user";
+            break;        
+        case userSelection === "paper" && computerSelection === "rock":
+            console.log("You win! Paper beats rock");
+            winner = "user";
+            break;
+        case userSelection === "paper" && computerSelection === "paper":
+            console.log("Tie! You both chose paper");
+            winner = "tie";
+            break;
+        case userSelection === "paper" && computerSelection === "scissors":
+            console.log("You lose! Scissors beats paper");
+            winner = "computer";
+            break;
+        case userSelection === "scissors" && computerSelection === "rock":
+            console.log("You lose! Rock beats scissors");
+            winner = "computer";
+            break;
+        case userSelection === "scissors" && computerSelection === "paper":
+            console.log("You win! Scissors beats paper");
+            winner = "user";
+            break;
+        case userSelection === "scissors" && computerSelection === "scissors":
+            console.log("Tie! You both chose scissors");
+            winner = "tie";
+            break;    
+    }
+    return winner;
+}
+
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    let i;
+    for (i = 0; i < 5; i++) {
+        let winner = playRound();
+        if (winner === "user") {
+            userScore += 1;
+        }
+        else if (winner === "computer") {
+            computerScore += 1;
+        }
+        if (i < 4) {
+            console.log(`The current score is User: ${userScore}, Computer: ${computerScore}`);
+        }
+        
+    }
+    console.log(`Game Over! The final score is User: ${userScore}, Computer: ${computerScore}`);
 }
